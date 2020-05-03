ca2724f0697933a665ecfc6956409cc1d9230f2e
MikeFischer1 <fischems@lafayette.edu>
2020-05-03 19:20:00 +0000

    Full poistional tracking implemented, with some bugs

--- cvproc.py
+++ cvproc.py
@@ -4,58 +4,66 @@ roslib.load_manifest('dcam_description')
 import sys
 import rospy
 import cv2
-from std_msgs.msg import String
+from std_msgs.msg import String,Int16MultiArray
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
 class image_converter:
-    def __init__(self):
-        ## Output image with circle
-        self.image_pub = rospy.Publisher("cv_output",Image)
+	def __init__(self):
+		self.pixel_array = Int16MultiArray()
+		self.pixel_array.data = []
+		## Output image with circle
+		self.image_pub = rospy.Publisher("cv_output",Image)
+		self.pix_array = rospy.Publisher("yz_pix",Int16MultiArray)
 
-        ## CV Bridge the raw image in
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/depth_camera_ir/depth_camera/color/image_raw",Image,self.callback)
+		## CV Bridge the raw image in
+		self.bridge = CvBridge()
+		self.image_sub = rospy.Subscriber("/depth_camera_ir/depth_camera/color/image_raw",Image,self.callback)
 
-    def callback(self,data):
-        ## Convert raw image to CV-usable data, known as cv_image
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
+	def callback(self,data):
+		## Convert raw image to CV-usable data, known as cv_image
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+		except CvBridgeError as e:
+			print(e)
 
-        gimg = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
-        gimg = cv2.medianBlur(cv_image,5)
-        #edge filter to get a black and white image
-        canny = cv2.Canny(gimg, 200, 200)
-        #detector must work only on canny (edge) image.
-        circles = cv2.HoughCircles(canny,cv2.cv.CV_HOUGH_GRADIENT,1,20,
-                            param1=90,param2=20,minRadius=5,maxRadius=200)
-        print cv_image.shape
-        circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-        # draw the outer circle
-            cv2.circle(cv_image,(i[0],i[1]),i[2],(0, 255, 0),2)
-        # draw the center of the circle
-            cv2.circle(cv_image,(i[0],i[1]),2,(0,0,255),3)
-        print i[0]
-        print i[1]
-        # Output cv_image and canny to jpg files for viewing
-        cv2.imwrite("output.jpg",cv_image)
-        cv2.imwrite("canny.jpg",canny)
+		gimg = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
+		gimg = cv2.medianBlur(cv_image,5)
+		#edge filter to get a black and white image
+		canny = cv2.Canny(gimg, 200, 200)
+		#detector must work only on canny (edge) image.
+		circles = cv2.HoughCircles(canny,cv2.cv.CV_HOUGH_GRADIENT,1,20,
+							param1=90,param2=20,minRadius=5,maxRadius=30)
+		print cv_image.shape
+		circles = np.uint16(np.around(circles))
+		for i in circles[0,:]:
+		# draw the outer circle
+			cv2.circle(cv_image,(i[0],i[1]),i[2],(0, 255, 0),2)
+		# draw the center of the circle
+			cv2.circle(cv_image,(i[0],i[1]),2,(0,0,255),3)
+		print i[0]
+		print i[1]
+		self.pixel_array.data = []
+		self.pixel_array.data.append(i[0])
+		self.pixel_array.data.append(i[1])
+		self.pix_array.publish(self.pixel_array)
+
+		# Output cv_image and canny to jpg files for viewing
+		cv2.imwrite("output.jpg",cv_image)
+		cv2.imwrite("canny.jpg",canny)
 
 
 
 def main(args):
-    ic = image_converter()
-    rospy.init_node('image_converter', anonymous=True)
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
-    cv2.destroyAllWindows()
+	ic = image_converter()
+	rospy.init_node('image_converter', anonymous=True)
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print("Shutting down")
+	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main(sys.argv)
+	main(sys.argv)
--- depthloctest.py
+++ depthloctest.py
@@ -6,18 +6,37 @@ import cv2
 import cv2.cv
 import os
 import numpy as np
-from std_msgs.msg import String,Int32,Float32MultiArray
-from sensor_msgs.msg import Image,PointCloud2
+import time
+from std_msgs.msg import String,Int32,Float32MultiArray,Int16MultiArray
+from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 
 
 class depth_finder:
 	def __init__(self):
-		self.image_pub = rospy.Publisher("depth_array",Float32MultiArray)
+		self.xyz_array = Float32MultiArray()
+		self.xyz_array.data = []
+
+		self.dT = 1
+
+		#rospy.Timer(rospy.Duration(self.dT), self.loop, oneshot=False)
+
+
+		self.x = 0.0
+		self.y = 0.0
+		self.z = 0.0
+
+		self.r_v = 0.0
+
+		self.pix_y = 0
+		self.pix_z = 0
+
+		self.xyz_pub = rospy.Publisher('button_xyz',Float32MultiArray)
 		self.bridge = CvBridge()
         ## Bring in point cloud from message PointCloud2
 		self.image_sub = rospy.Subscriber("/depth_camera_ir/depth_camera/depth/image_raw",Image,self.callback)
+		self.pix_array = rospy.Subscriber("/yz_pix",Int16MultiArray,self.pixel_callback)
 
 	def callback(self,data):
         ## Convert depth image to cv2 image
@@ -27,26 +46,50 @@ class depth_finder:
 		
 
 		depth_array = np.array(depth_image, dtype=np.float32)
-		
-		print depth_array.shape
-		print depth_array[400,400]
 
-		#this_folder = os.path.dirname(__file__)
-		#data_file = os.path.join(this_folder,'depth_proc.txt')
-		#np.set_printoptions(threshold=sys.maxsize)
-		#with open(data_file, 'w') as f:
-		#	np.set_printoptions(threshold=np.inf)
-		#	f.write('{}'.format(depth_array))
+		print depth_array.shape
+
+		
+		self.x = depth_array[self.pix_z,self.pix_y]
+
+		self.r_v = self.x*np.sin(0.523599)
+		self.y = (300-float(self.pix_y))*self.r_v/300
+		self.z = (300-float(self.pix_z))*self.r_v/300
+
+		self.xyz_array.data = []
+		self.xyz_array.data.append(self.x)
+		self.xyz_array.data.append(self.y)
+		self.xyz_array.data.append(self.z)
+		self.xyz_pub.publish(self.xyz_array)
+		
+		print self.x
+		print self.y
+		print self.z
+
+	def pixel_callback(self,data):
+		
+		self.pix_y = data.data[0]
+		self.pix_z = data.data[1]
+	
+	#def loop(self,event):
+
+		#self.xyz_array.data = []
+		#self.xyz_array.data.append(self.x)
+		#self.xyz_array.data.append(self.y)
+		#self.xyz_array.data.append(self.z)
+		#self.xyz_pub.publish(self.xyz_array)
+
+		
 
 
 def main(args):
-    df = depth_finder()
-    rospy.init_node('depth_finder', anonymous=True)
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print "Shutting down"
-    cv2.destroyAllWindows()
+	df = depth_finder()
+	rospy.init_node('depth_finder', anonymous=True)
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print "Shutting down"
+	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main(sys.argv)
+	main(sys.argv)
